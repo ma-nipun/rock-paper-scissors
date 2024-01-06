2a022a593ee7d327545ca5fb20902fe30df27352
Mahela Indeewara <90456570+ma-nipun@users.noreply.github.com>
2024-01-06 21:59:57 +0530

    now it plays a round of rock paper scissors Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
+const cars = ["rock", "paper", "scissor"]
+
 function getComputerChoice() {
-    const cars = ["Rock", "Paper", "Scissor"]
     let choice = Math.floor((Math.random() * 10) + 1)%3
     console.log(cars[choice])
-    // return p1 * p2;
+    return cars[choice];
   }
 
-  getComputerChoice()
+function getUserChoice(){
+    let name = prompt("Your Choice");
+    if (cars.indexOf(name.toLocaleLowerCase()) !==-1)
+    {
+        return name.toLocaleLowerCase();
+    }
+    else
+    {
+        getUserChoice()
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    // Ensure the input is in lowercase for case-insensitive comparison
+
+    // Check for a tie
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+
+    // Check for Rock
+    if (playerSelection === 'rock') {
+        return (computerSelection === 'scissors') ? 'You Win!' : 'Computer Win!';
+    }
+
+    // Check for Paper
+    if (playerSelection === 'paper') {
+        return (computerSelection === 'rock') ? 'You Win!' : 'Computer Win!';
+    }
+
+    // Check for Scissors
+    if (playerSelection === 'scissors') {
+        return (computerSelection === 'paper') ? 'You Win!' : 'Computer Win!';
+    }
+  }
+
+//   getComputerChoice()
+// getUserChoice()
+console.log(playRound("rock",getComputerChoice()) )
